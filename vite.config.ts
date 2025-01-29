379c09d12d5ee3736a66d50edd6d40e299f472e2
Aritra Ghorai <aritra1521@gmail.com>
2025-01-29 15:29:23 +0530

    added vite optimization image

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from "vite";
 import { reactRouter } from "@react-router/dev/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRouter(), tsconfigPaths()],
+  plugins: [reactRouter(), tsconfigPaths(), ViteImageOptimizer()],
 });
